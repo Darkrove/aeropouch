0d24c1b6b3d1be857780abac699d18804912662f
sajjad <samaralishaikh212@gmail.com>
2023-12-26 19:51:00 +0530

    change button component to link

--- src/components/no-product.tsx
+++ src/components/no-product.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import Link from "next/link";
 import Paragraph from "@ui/paragraph";
 import LargeHeading from "@ui/large-heading";
-import { Button } from "@ui/button";
+import { Button, buttonVariants } from "@ui/button";
 import { Icons } from "@/components/icons";
 
 interface NoProductProps {}
@@ -16,8 +16,11 @@ const NoProduct: FC<NoProductProps> = ({}) => {
         <Paragraph className="text-gray-500 mt-2 text-center">
           You can try our different products.
         </Paragraph>
-        <Link href="/categories/all">
-          <Button variant="outline">View Other Products</Button>
+        <Link
+          href="/categories/all"
+          className={buttonVariants({ variant: "outline" })}
+        >
+          View Other Products
         </Link>
       </div>
     </div>
