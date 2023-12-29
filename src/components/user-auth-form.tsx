6d08fe310f9807b1e7c3e2578a55d6df07a0fe97
sajjad <samaralishaikh212@gmail.com>
2023-12-29 20:28:46 +0530

    showing loading status

--- src/components/user-auth-form.tsx
+++ src/components/user-auth-form.tsx
@@ -33,6 +33,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   });
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const [isError, setIsError] = React.useState<boolean>(false);
+  const [isExchangeToken, setIsExchangeToken] = React.useState<boolean>(false);
   const [message, setMessage] = React.useState<string | null>(null);
   const searchParams = useSearchParams();
   const router = useRouter();
@@ -45,7 +46,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
       }
 
       try {
-        setIsLoading(true);
+        setIsExchangeToken(true);
         const jwt = await getToken(token);
         if (jwt) {
           setCustomer();
@@ -62,7 +63,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
             "There was an error logging in with Email, Please retry!",
         });
       } finally {
-        setIsLoading(false);
+        setIsExchangeToken(false);
       }
     };
 
@@ -102,36 +103,42 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-              {...register("email")}
-            />
-            {errors?.email && (
-              <p className="px-1 text-xs text-red-600">
-                {errors.email.message}
-              </p>
-            )}
-          </div>
-          <button className={cn(buttonVariants())} disabled={isLoading}>
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Sign In with Email
-          </button>
-        </div>
-      </form>
+      {isExchangeToken ? (
+        <>Logging....</>
+      ) : (
+        <>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid gap-2">
+              <div className="grid gap-1">
+                <Label className="sr-only" htmlFor="email">
+                  Email
+                </Label>
+                <Input
+                  id="email"
+                  placeholder="name@example.com"
+                  type="email"
+                  autoCapitalize="none"
+                  autoComplete="email"
+                  autoCorrect="off"
+                  disabled={isLoading}
+                  {...register("email")}
+                />
+                {errors?.email && (
+                  <p className="px-1 text-xs text-red-600">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+              <button className={cn(buttonVariants())} disabled={isLoading}>
+                {isLoading && (
+                  <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                )}
+                Sign In with Email
+              </button>
+            </div>
+          </form>
+        </>
+      )}
     </div>
   );
 }
